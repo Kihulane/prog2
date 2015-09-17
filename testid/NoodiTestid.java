d9d845495ed4e43a6feab09f6da326f28f6356cb
Gert Adamson <gert.adamson@tigu.hk.tlu.ee>
2015-09-17 12:40:46 +0300

    test

---
+++ testid/NoodiTestid.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class NoodiTestid{
+	@Test
+	public void loomine(){
+		Noot n1=new Noot("A");
+	}
+	@Test
+	public void korgus(){
+		Noot n1=new Noot("A");
+		assertEquals(69, n1.kysiKorgus());
+	}
+}
